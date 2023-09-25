972a7888f45a2b6a72adcbce65149cdc915bcd50
Sebastian <hawaiilive@outlook.com>
2023-09-25 19:45:16 +0200

    add button style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/shadcn/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen flex justify-center items-center">
-      <Link href={"/profile"}>Till din profil</Link>
+      <Button>
+        <Link href={"/profile"}>Till din profil</Link>
+      </Button>
     </main>
   );
 }
